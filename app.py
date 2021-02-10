5ca34889739a8dd7fa1ccaefeaa11f6dadeb4227
Sergey Biryukov <serb@codepros.ru>
2021-02-10 15:01:23 +0300

    V2 release

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import random as r
 
 app = Flask(__name__)
+
+# define global words dictionary
+words = {}
+words["easy"] = []
+words["medium"] = []
+words["hard"] = []
+
+# load words
+file = open("word_rus_short.txt", encoding="UTF8")
+for i, word in enumerate(file):
+    word = word.rstrip('\n')
+    if len(word) <= 4:
+        words["easy"].append(word)
+    if len(word) >= 5 and len(word) <= 8:
+        words["medium"].append(word)
+    if len(word) > 8:
+        words["hard"].append(word)
+file.close()
 
 
 def read_word():
@@ -16,14 +34,36 @@ def read_word():
 
 
 def scramble_word(word):
+    scramble = word
     l = list(word)
-    r.shuffle(l)
-    return "".join(l)
+    while scramble == word:
+        r.shuffle(l)
+        scramble = "".join(l)
+    return scramble
 
 
 @app.route('/')
-def hello_world():
-    word = read_word()
+def index():
+    rand = r.randint(0, len(words["medium"]))
+    word = words["medium"][rand]
     scrable = scramble_word(word)
     result = {"word": word, "scramble": scrable}
     return render_template("index.html", result=result)
+
+
+@app.route('/next/<level>')
+def next(level):
+    l = int(level)
+    word = ""
+    if l == 1:
+        rand = r.randint(0, len(words["easy"]))
+        word = words["easy"][rand]
+    if l == 2:
+        rand = r.randint(0, len(words["medium"]))
+        word = words["medium"][rand]
+    if l == 3:
+        rand = r.randint(0, len(words["hard"]))
+        word = words["hard"][rand]
+    scramble = scramble_word(word)
+    result = {"word": word, "scramble": scramble}
+    return jsonify(result)
